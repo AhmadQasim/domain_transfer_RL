ea48b05b7182bb58b849e32f6d3cde3af01c24d8
ahmad.qasim <ahmad.live.1@gmail.com>
2020-02-05 02:08:44 +0100

    commenting the ar model api class

--- demand_models/ar_demand_predictor.py
+++ demand_models/ar_demand_predictor.py
@@ -12,6 +12,15 @@ class AutoRegressiveDemandPredictor:
                  model_path=None,
                  load_model=False):
 
+        """
+        :param config_path: inventory.yaml file path
+        :param steps: episode_max_steps in the inventory.yaml file
+        :param days: number of days to sample for poisson for training
+        :param bins_size: the bin size i.e. number of steps in each bin
+        :param model_path: the path where the model is saved and loaded from
+        :param load_model: load already saved model
+        """
+
         self.model_path = model_path
 
         if load_model:
@@ -25,6 +34,12 @@ class AutoRegressiveDemandPredictor:
         self.lag = self.ar.model_fit.k_ar
 
     def predict(self, curr_data, pred_steps, item):
+        """
+        :param curr_data: the new data received
+        :param pred_steps: number of predictions to be returned
+        :param item: the product ID of curr_data and prediction data, according to inventory.yaml
+        :return: predictions, numpy array
+        """
         predictions = self.ar.predict_next_n(curr_data=curr_data, pred_steps=pred_steps, item=item)
         self.ar.save_models()
 
